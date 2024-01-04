7c370e51ed3d2fbbf1bccd5345d0e69e79762a8a
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2024-01-04 20:35:25 -0300

    Property Binding

--- src/app/components/template/tamplate-binding/tamplate-binding.component.ts
+++ src/app/components/template/tamplate-binding/tamplate-binding.component.ts
@@ -11,4 +11,17 @@ import { Component } from '@angular/core';
 export class TamplateBindingComponent {
   public name = 'Erivaldo Pereira de Santana';
   public age = 37
+  public condition = this.age > 1 ? 'Teste' : 'Teste2';
+
+  public isDesabled = false;
+  public srcValue = "https://vidafullstack.com.br/wp-content/uploads/2022/04/Aprenda-o-que-e-Databinding-com-Angular-1080x675.jpg";
+
+  constructor(){
+    setTimeout( ()=>{
+      this.name = 'João e Maria';
+    }, 1000);
+  }
+  public sum(val1: number, val2: number){
+    return val1 + val2;
+  }
 }
